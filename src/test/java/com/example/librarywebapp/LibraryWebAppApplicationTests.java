5c4aaa6fddda71b0c53a0400b105087b0e12757f
bhanu3023 <bhanuprakash80963@gmail.com>
2025-05-07 11:59:24 +0530

    Create LibraryWebAppApplicationTests.java

---
+++ src/test/java/com/example/librarywebapp/LibraryWebAppApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.librarywebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
